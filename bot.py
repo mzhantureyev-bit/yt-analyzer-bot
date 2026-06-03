d0a4bd9e91734c0bea68f811192b3aa2faa035b3
mzhantureyev-bit <mzhantureyev@gmail.com>
2026-06-03 14:57:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import Application, MessageHandler, CommandHandler, filters, C
 
 TELEGRAM_TOKEN = "8879366892:AAGSozS7aaADKosbT0qS29CFK9GHUl4ydhM"
 GEMINI_API_KEY = "AQ.Ab8RN6ITlRIpSVYi_M1dSS0EC4ZtsTTuDzwKiQFoohjyzyKUMw"
-GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + GEMINI_API_KEY
+GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" + GEMINI_API_KEY
 
 def extract_video_id(url):
     m = re.search(r'(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/)([a-zA-Z0-9_-]{11})', url)
